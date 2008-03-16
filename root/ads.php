c6d06989976ad0421bcff860d4ac0f0448737905
exreaction@gmail.com <exreaction@gmail.com@01084517-8548-0410-843f-51b8fe06b3f6>
2008-03-16 03:43:36 +0000

    Actually, I am going to setup the install differently...
    Setting up some more files.
    
    git-svn-id: http://phpbb3-advertisement-management.googlecode.com/svn/trunk@5 01084517-8548-0410-843f-51b8fe06b3f6

---
+++ root/ads.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* @package phpBB3 Advertisement Management
+* @version $Id$
+* @copyright (c) 2008 EXreaction, Lithium Studios
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+$ads_version = '0.3.0';
+
+// Stuff required to work with phpBB3
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup('mods/ads');
+
+if (!isset($config['ads_version']))
+{
+	
+}
+?>
---
+++ root/ads/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+*
+* @package phpBB3 Advertisement Management
+* @version $Id$
+* @copyright (c) 2008 EXreaction, Lithium Studios
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+?>
