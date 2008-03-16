7270c338f727e537d596fb8008c8c7da6da747b2
exreaction@gmail.com <exreaction@gmail.com@01084517-8548-0410-843f-51b8fe06b3f6>
2008-03-16 03:47:00 +0000

    Id keywords shouldn't be added like they were...hopefully they won't be re-added.
    git-svn-id: http://phpbb3-advertisement-management.googlecode.com/svn/trunk@7 01084517-8548-0410-843f-51b8fe06b3f6

--- root/create_schema_files.php
+++ root/create_schema_files.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package phpBB3
-* @version $Id$ create_schema_files.php 241 2008-02-09 03:33:11Z exreaction@gmail.com $
+* @version $Id$
 * @copyright (c) 2006 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
