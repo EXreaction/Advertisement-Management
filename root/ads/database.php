32c363e671f1ba7033f350b925f94732a86e91d5
exreaction@gmail.com <exreaction@gmail.com@01084517-8548-0410-843f-51b8fe06b3f6>
2009-03-08 02:35:31 +0000

    If ever needed again for debugging...
    git-svn-id: http://phpbb3-advertisement-management.googlecode.com/svn/trunk@82 01084517-8548-0410-843f-51b8fe06b3f6

---
+++ root/ads/database.php
@@ -0,0 +1,24 @@
+<?php
+define('UMIL_AUTO', true);
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+if (!file_exists($phpbb_root_path . 'umil/umil_auto.' . $phpEx))
+{
+	trigger_error('Please download the latest UMIL (Unified MOD Install Library) from: <a href="http://www.phpbb.com/mods/umil/">phpBB.com/mods/umil</a>', E_USER_ERROR);
+}
+
+$mod_name = 'Advertisement Management';
+$version_config_name = 'ads_version';
+$language_file = 'mods/ads';
+
+include($phpbb_root_path . 'ads/versions.' . $phpEx);
+
+include($phpbb_root_path . 'umil/umil_auto.' . $phpEx);
+
+?>
