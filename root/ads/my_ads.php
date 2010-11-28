9c285cb9aa32d25344b731dbbba43655c080187b
EXreaction <exreaction@phpbb.com>
2010-11-28 12:29:54 -0600

    Prevent an SQL error when the user has no ads set to their account

--- root/ads/my_ads.php
+++ root/ads/my_ads.php
@@ -36,26 +36,6 @@ $template->assign_vars(array(
 	'PAGE_TITLE'		=> $user->lang['ACP_ADVERTISEMENT_MANAGEMENT'],
 ));
 
-// Positions
-$positions = array();
-$sql = 'SELECT position_id, lang_key FROM ' . ADS_POSITIONS_TABLE . ' ORDER BY position_id ASC';
-$result = $db->sql_query($sql);
-while ($row = $db->sql_fetchrow($result))
-{
-	$positions[$row['position_id']] = (isset($user->lang[$row['lang_key']])) ? $user->lang[$row['lang_key']] : $row['lang_key'];
-}
-$db->sql_freeresult($result);
-
-// Forums
-$forums = array();
-$sql = 'SELECT forum_id, forum_name FROM ' . FORUMS_TABLE . ' ORDER BY forum_id ASC';
-$result = $db->sql_query($sql);
-while ($row = $db->sql_fetchrow($result))
-{
-	$forums[$row['forum_id']] = $row['forum_name'];
-}
-$db->sql_freeresult($result);
-
 // Advertisements
 $ads = array();
 $sql = 'SELECT * FROM ' . ADS_TABLE . '
@@ -70,43 +50,66 @@ while ($row = $db->sql_fetchrow($result))
 }
 $db->sql_freeresult($result);
 
-$sql = 'SELECT * FROM ' . ADS_IN_POSITIONS_TABLE . '
-	WHERE ' . $db->sql_in_set('ad_id', array_keys($ads));
-$result = $db->sql_query($sql);
-while ($row = $db->sql_fetchrow($result))
+if (sizeof($ads))
 {
-	$ads[$row['ad_id']]['positions'][] = $positions[$row['position_id']];
-}
-$db->sql_freeresult($result);
+	// Positions
+	$positions = array();
+	$sql = 'SELECT position_id, lang_key FROM ' . ADS_POSITIONS_TABLE . ' ORDER BY position_id ASC';
+	$result = $db->sql_query($sql);
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$positions[$row['position_id']] = (isset($user->lang[$row['lang_key']])) ? $user->lang[$row['lang_key']] : $row['lang_key'];
+	}
+	$db->sql_freeresult($result);
 
-$sql = 'SELECT * FROM ' . ADS_FORUMS_TABLE . '
-	WHERE ' . $db->sql_in_set('ad_id', array_keys($ads));
-$result = $db->sql_query($sql);
-while ($row = $db->sql_fetchrow($result))
-{
-	$ads[$row['ad_id']]['forums'][] = $forums[$row['forum_id']];
-}
-$db->sql_freeresult($result);
+	// Forums
+	$forums = array();
+	$sql = 'SELECT forum_id, forum_name FROM ' . FORUMS_TABLE . ' ORDER BY forum_id ASC';
+	$result = $db->sql_query($sql);
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$forums[$row['forum_id']] = $row['forum_name'];
+	}
+	$db->sql_freeresult($result);
 
-foreach ($ads as $row)
-{
-	$ads_in_positions = implode('<br />', $row['positions']);
-	$ads_in_forums = implode('<br />', $row['forums']);
+	$sql = 'SELECT * FROM ' . ADS_IN_POSITIONS_TABLE . '
+		WHERE ' . $db->sql_in_set('ad_id', array_keys($ads));
+	$result = $db->sql_query($sql);
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$ads[$row['ad_id']]['positions'][] = $positions[$row['position_id']];
+	}
+	$db->sql_freeresult($result);
 
-	$template->assign_block_vars('ads', array(
-		'AD_ID'				=> $row['ad_id'],
-		'AD_ENABLED'		=> ($row['ad_enabled']) ? $user->lang['TRUE'] : $user->lang['FALSE'],
-		'AD_CODE'			=> $row['ad_code'],
-		'AD_CODE_DISPLAY'	=> htmlspecialchars_decode($row['ad_code']),
-		'AD_TIME'			=> date('d F Y', $row['ad_time']),
-		'AD_TIME_END'		=> ($row['ad_time_end']) ? date('d F Y', $row['ad_time_end']) : 0,
-		'AD_VIEW_LIMIT'		=> $row['ad_view_limit'],
-		'AD_VIEWS'			=> $row['ad_views'],
-		'AD_CLICK_LIMIT'	=> $row['ad_click_limit'],
-		'AD_CLICKS'			=> ($row['ad_clicks']) ? $row['ad_clicks'] : $user->lang['0_OR_NA'],
-		'AD_IN_POSITIONS'	=> $ads_in_positions,
-		'AD_IN_FORUMS'		=> ($row['all_forums']) ? $user->lang['ALL_FORUMS'] : $ads_in_forums,
-	));
+	$sql = 'SELECT * FROM ' . ADS_FORUMS_TABLE . '
+		WHERE ' . $db->sql_in_set('ad_id', array_keys($ads));
+	$result = $db->sql_query($sql);
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$ads[$row['ad_id']]['forums'][] = $forums[$row['forum_id']];
+	}
+	$db->sql_freeresult($result);
+
+	foreach ($ads as $row)
+	{
+		$ads_in_positions = implode('<br />', $row['positions']);
+		$ads_in_forums = implode('<br />', $row['forums']);
+
+		$template->assign_block_vars('ads', array(
+			'AD_ID'				=> $row['ad_id'],
+			'AD_ENABLED'		=> ($row['ad_enabled']) ? $user->lang['TRUE'] : $user->lang['FALSE'],
+			'AD_CODE'			=> $row['ad_code'],
+			'AD_CODE_DISPLAY'	=> htmlspecialchars_decode($row['ad_code']),
+			'AD_TIME'			=> date('d F Y', $row['ad_time']),
+			'AD_TIME_END'		=> ($row['ad_time_end']) ? date('d F Y', $row['ad_time_end']) : 0,
+			'AD_VIEW_LIMIT'		=> $row['ad_view_limit'],
+			'AD_VIEWS'			=> $row['ad_views'],
+			'AD_CLICK_LIMIT'	=> $row['ad_click_limit'],
+			'AD_CLICKS'			=> ($row['ad_clicks']) ? $row['ad_clicks'] : $user->lang['0_OR_NA'],
+			'AD_IN_POSITIONS'	=> $ads_in_positions,
+			'AD_IN_FORUMS'		=> ($row['all_forums']) ? $user->lang['ALL_FORUMS'] : $ads_in_forums,
+		));
+	}
 }
 
 $template->set_filenames(array(
